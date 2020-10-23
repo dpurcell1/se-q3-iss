c523043b6963b77c0fe198cfba1e8b6bba8bd5bc
dpurcell1 <dpsvr3@gmail.com>
2020-10-23 18:26:30 -0400

    Initial commit for review

--- iss.py
+++ iss.py
@@ -1,10 +1,61 @@
 #!/usr/bin/env python
+import requests
+import time
+import turtle
 
-__author__ = '???'
+__author__ = 'Darrell Purcell'
+
+
+def in_space():
+    r = requests.get('http://api.open-notify.org/astros.json')
+    space_dict = r.json()
+    people = space_dict['people']
+    total_in_space = space_dict['number']
+    for person in people:
+        print('Name: ' + person['name'])
+        print('Craft: ' + person['craft'])
+    print(f'Total astronauts in space: {total_in_space}')
+
+
+def iss_data():
+    r = requests.get('http://api.open-notify.org/iss-now.json')
+    api_dict = r.json()
+    coords = api_dict['iss_position']
+    x = float(coords['longitude'])
+    y = float(coords['latitude'])
+    timestamp = api_dict['timestamp']
+    return [x, y, timestamp]
+
+
+def render_map(x, y):
+    r = 'http://api.open-notify.org/iss-pass.json?lat=39.76838&lon=-86.15804'
+    passes_obj = requests.get(r)
+    passes_data = passes_obj.json()
+    pass_time = time.ctime(passes_data['response'][0]['risetime'])
+
+    marker = turtle.Turtle()
+    indy_x = -86.15804
+    indy_y = 39.76838
+    screen = turtle.Screen()
+
+    screen.setup(width=720, height=360)
+    screen.bgpic('map.gif')
+    screen.setworldcoordinates(-720, -360, 720, 360)
+    screen.register_shape('iss.gif')
+    marker.penup()
+    marker.goto(indy_x, indy_y)
+    marker.dot(10, 'yellow')
+    marker.write(pass_time)
+    marker.shape('iss.gif')
+    marker.penup()
+    marker.goto(x, y)
+    turtle.done()
 
 
 def main():
-    pass
+    in_space()
+    coords = iss_data()
+    render_map(coords[0], coords[1])
 
 
 if __name__ == '__main__':
